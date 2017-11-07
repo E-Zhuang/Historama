15865806932a83f1b6837965ffc9d94e40b106f1
Emily Zhuang <Emily@Richards-MacBook-Pro.local>
2017-11-07 10:04:56 -0800

    Initial Commit

---
+++ Historama/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Historama
+//
+//  Created by Emily Zhuang on 11/7/17.
+//  Copyright © 2017 Emily Zhuang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
